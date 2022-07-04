2c80e695b12a5f9874173629666723be71fc93cf
Bohdan Obodovskyi <bogdan.o@done-it.net>
2022-07-04 20:06:29 +0300

    annotate category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  image       :string
+#  category_id :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Category < ApplicationRecord
   belongs_to :category, optional: true
 
