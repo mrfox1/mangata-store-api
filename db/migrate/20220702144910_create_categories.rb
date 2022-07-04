6b6b79044772c68b1c9b4a39b098f3e91fbb0cd6
Bohdan Obodovskyi <bogdan.o@done-it.net>
2022-07-04 20:04:03 +0300

    fix categories model; add seeds

--- db/migrate/20220702144910_create_categories.rb
+++ db/migrate/20220702144910_create_categories.rb
@@ -3,7 +3,7 @@ class CreateCategories < ActiveRecord::Migration[7.0]
     create_table :categories do |t|
       t.string :title
       t.string :image
-      t.references :category, null: false, foreign_key: true
+      t.references :category, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_02_144910) do
   create_table "categories", force: :cascade do |t|
     t.string "title"
     t.string "image"
-    t.bigint "category_id", null: false
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_categories_on_category_id"
