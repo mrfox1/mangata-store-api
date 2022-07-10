95441ec3ad11cb44508309bcdd867d99f68ef953
Bohdan Obodovskyi <bogdan.o@done-it.net>
2022-07-10 15:52:59 +0300

    add methods for render errors

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -4,5 +4,19 @@ module Api
   class ApiController < ApplicationController
     # helper_method SessionService.instance_methods
     rescue_from ActiveRecord::RecordNotFound, with: :not_found_record
+
+    protected
+
+    def render_errors(errors, status = :unprocessable_entity)
+      render json: { errors: (errors.is_a?(Array) ? errors : [errors]) }, status: status
+    end
+
+    def render_error(error, status = :unprocessable_entity)
+      render json: { error: error }, status: status
+    end
+
+    def not_found_record(exception)
+      render_errors "#{ exception.model } #{ I18n.t('errors.not_found') }", :not_found
+    end
   end
 end
