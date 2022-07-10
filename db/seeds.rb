1dce0260f2915caa55def8e5f4e0a61c7db9a0df
Bohdan Obodovskyi <bogdan.o@done-it.net>
2022-07-10 15:43:34 +0300

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
 if Rails.env.development?
-  Category.create(title: 'Candles')
-  Category.create(title: 'Perfumes')
+  Category.create(title: 'Candles', attachment_attributes: { remote_attachment_url: 'https://www.ikea.com/gb/en/images/products/fenomen-unscented-block-candle-natural__0904463_pe586190_s5.jpg' })
+  Category.create(title: 'Perfumes', attachment_attributes: { remote_attachment_url: 'https://irecommend.ru/sites/default/files/product-images/158247/HZKrJ6tS5mPLe15FiyM1DA.png' })
 end
