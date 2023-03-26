fac1a7802e16fb9d4041a82a6a2994387c3dd54c
Bohdan Obodovskyi <lsdovscky@gmail.com>
2023-03-26 17:53:04 +0300

    add in_stock field to Product
    Signed-off-by: Bohdan Obodovskyi <lsdovscky@gmail.com>

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_25_165804) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_25_170933) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -70,6 +70,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_25_165804) do
     t.datetime "updated_at", null: false
     t.bigint "price"
     t.integer "discount"
+    t.boolean "in_stock"
     t.index ["category_id"], name: "index_products_on_category_id"
   end
 
