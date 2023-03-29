1f0239a7d9a787748f834fe73f5ffac66388690e
Bohdan Obodovskyi <lsdovscky@gmail.com>
2023-03-29 19:33:49 +0300

    add annotatets to factories
    Signed-off-by: Bohdan Obodovskyi <lsdovscky@gmail.com>

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: categories
+#
+#  id          :bigint           not null, primary key
+#  title       :string
+#  image       :string
+#  category_id :bigint
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 FactoryBot.define do
   factory :category do
     title { 'Candles' }
--- spec/factories/product_options.rb
+++ spec/factories/product_options.rb
@@ -1,3 +1,13 @@
+# == Schema Information
+#
+# Table name: product_options
+#
+#  id           :bigint           not null, primary key
+#  option_type  :integer
+#  option_value :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
 FactoryBot.define do
   factory :product_option do
     option_type { 0 }
